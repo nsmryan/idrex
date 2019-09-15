ddf4be4a94ab05bea1ca2c8a0913baf9d5949677
nsmryan <nsmryan@gmail.com>
2019-09-15 13:12:43 -0400

    Split font and map info calculations into update function instead of all in draw function

--- src/state.rs
+++ src/state.rs
@@ -96,6 +96,9 @@ pub struct MainState {
     pub tile_image: XpFile,
     pub params: Params,
     pub info: Info,
+    pub map_disp: Rect,
+    pub font_disp: Rect,
+    pub char_disp: Rect,
 }
 
 impl MainState {
@@ -109,6 +112,9 @@ impl MainState {
             tile_image,
             params: Params::default(),
             info: Info::default(),
+            map_disp: Rect::default(),
+            font_disp: Rect::default(),
+            char_disp: Rect::default(),
         };
         Ok(s)
     }
@@ -138,8 +144,81 @@ pub fn hightlight_square(ctx: &mut Context, pos: Point2<f32>, width: f32, height
     Ok(())
 }
 
+pub fn calc_map_scaler(map_disp: Rect, map_width: f32, map_height: f32) -> f32 {
+    let map_scaler;
+    if (map_disp.w / map_width) <= map_disp.h {
+        map_scaler = map_disp.w / map_width;
+    } else {
+        map_scaler = map_disp.h / map_height;
+    }
+
+    return map_scaler;
+}
+
 impl EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let mouse_pos = ggez::input::mouse::position(ctx);
+
+        /* Calculate dimensions of each component of the screen */
+
+        let screen_coords = ggez::graphics::screen_coordinates(ctx);
+        self.map_disp = screen_coords;
+        self.map_disp.scale(0.5, 1.0);
+        let map_disp_width = self.map_disp.w;
+        let map_disp_height = self.map_disp.h;
+        let map_width = self.tile_image.layers[0].width as f32 * 16.0;
+        let map_height = self.tile_image.layers[0].height as f32 * 16.0;
+        let map_scaler = calc_map_scaler(self.map_disp, map_width, map_height);
+        self.map_disp.scale(map_scaler, map_scaler);
+        self.map_disp.move_to([(map_disp_width - map_width * map_scaler) / 2.0,
+                               (map_disp_height -  map_height * map_scaler) / 2.0,]);
+
+        self.font_disp = screen_coords;
+        self.font_disp.scale(0.5, 0.5);
+        self.font_disp.move_to([screen_coords.w / 2.0, 0.0]);
+                              
+        self.char_disp = screen_coords;
+        self.char_disp.scale(0.5, 0.5);
+        self.char_disp.move_to([screen_coords.w / 2.0, screen_coords.h / 2.0]);
+
+        // Font Info
+        self.info.font_info = None;
+
+        let ch_width = self.font_disp.w as f32 / 16.0;
+        let ch_height = self.font_disp.h as f32 / 16.0;
+        if self.font_disp.contains(mouse_pos) {
+            // get character under cursor
+            let x = (((mouse_pos.x - self.font_disp.x) as f32 / self.font_disp.w as f32) * 16.0) as i32;
+            let y = (((mouse_pos.y - self.font_disp.y) as f32 / self.font_disp.h as f32) * 16.0) as i32;
+            let ch = std::char::from_u32((x + y * 16) as u32).unwrap();
+            self.info.font_info = Some(FontInfo::new(x, y, ch));
+        }
+
+        // Map Info
+        self.info.map_info = None;
+
+        for layer in self.tile_image.layers.iter() {
+            let tile_iter =
+                layer.cells.iter().enumerate().map(
+                    |(index, cell)| (index % layer.width, index / layer.width, cell)
+                 );
+
+            for (x, y, cell) in tile_iter {
+                let ch = std::char::from_u32(cell.ch).unwrap();
+                let pos = map_pos_to_screen(self.map_disp, x, y, map_scaler);
+
+                if (mouse_pos.x >= pos.x && mouse_pos.x < (pos.x + ch_width)) &&
+                   (mouse_pos.y >= pos.y && mouse_pos.y < (pos.y + ch_height)) {
+                        self.info.map_info =
+                            Some(MapInfo::new(ch, x, y));
+
+                    if self.info.font_info.is_none() {
+                        self.info.font_info = Some(FontInfo::new(x as i32, y as i32, ch));
+                    }
+                }
+            }
+        }
+
         Ok(())
     }
 
@@ -147,36 +226,11 @@ impl EventHandler for MainState {
         let background = ggez::graphics::Color::new(255.0 / 255.0, 140.0 / 255.0, 0.0, 1.0);
         ggez::graphics::clear(ctx, background);
 
-
-        /* Calculate dimensions of each component of the screen */
-        let screen_coords = ggez::graphics::screen_coordinates(ctx);
-
-        self.info.map_info = None;
-        self.info.font_info = None;
-
-        let mut map_disp = screen_coords;
-        map_disp.scale(0.5, 1.0);
-        let map_disp_width = map_disp.w;
-        let map_disp_height = map_disp.h;
         let map_width = self.tile_image.layers[0].width as f32 * 16.0;
         let map_height = self.tile_image.layers[0].height as f32 * 16.0;
-        let map_scaler;
-        if (map_disp.w / map_width) <= map_disp.h {
-            map_scaler = map_disp.w / map_width;
-        } else {
-            map_scaler = map_disp.h / map_height;
-        }
-        map_disp.scale(map_scaler, map_scaler);
-        map_disp.move_to([(map_disp_width - map_width * map_scaler) / 2.0,
-                          (map_disp_height -  map_height * map_scaler) / 2.0,]);
+        let map_scaler = calc_map_scaler(self.map_disp, map_width, map_height);
 
-        let mut font_disp = screen_coords;
-        font_disp.scale(0.5, 0.5);
-        font_disp.move_to([screen_coords.w / 2.0, 0.0]);
-                              
-        let mut char_disp = screen_coords;
-        char_disp.scale(0.5, 0.5);
-        char_disp.move_to([screen_coords.w / 2.0, screen_coords.h / 2.0]);
+        dbg!(map_scaler);
 
         // character to use in character display
         let mouse_pos = ggez::input::mouse::position(ctx);
@@ -184,22 +238,18 @@ impl EventHandler for MainState {
         // draw font display
         { 
             let params =
-                DrawParam::default().dest([font_disp.x, font_disp.y])
+                DrawParam::default().dest([self.font_disp.x, self.font_disp.y])
                                     // TODO the 256.0 should depend on the font image dimensions
-                                    .scale([font_disp.w / 256.0, font_disp.h / 256.0]);
+                                    .scale([self.font_disp.w / 256.0, self.font_disp.h / 256.0]);
             self.font_image.draw(ctx, params)?;
 
-            let ch_width = font_disp.w as f32 / 16.0;
-            let ch_height = font_disp.h as f32 / 16.0;
-            if font_disp.contains(mouse_pos) {
-                // get character under cursor
-                let x = (((mouse_pos.x - font_disp.x) as f32 / font_disp.w as f32) * 16.0) as i32;
-                let y = (((mouse_pos.y - font_disp.y) as f32 / font_disp.h as f32) * 16.0) as i32;
-                let ch = std::char::from_u32((x + y * 16) as u32).unwrap();
-                self.info.font_info = Some(FontInfo::new(x, y, ch));
-
-                let pos = Point2::from([font_disp.x + x as f32 * ch_width - 1.0,
-                                        font_disp.y + y as f32 * ch_height - 1.0]);
+            if let Some(font_info) = self.info.font_info {
+                let ch_width = self.font_disp.w as f32 / 16.0;
+                let ch_height = self.font_disp.h as f32 / 16.0;
+                let x = (((mouse_pos.x - self.font_disp.x) as f32 / self.font_disp.w as f32) * 16.0) as i32;
+                let y = (((mouse_pos.y - self.font_disp.y) as f32 / self.font_disp.h as f32) * 16.0) as i32;
+                let pos = Point2::from([self.font_disp.x + x as f32 * ch_width - 1.0,
+                                        self.font_disp.y + y as f32 * ch_height - 1.0]);
                 hightlight_square(ctx, pos, ch_width, ch_height, Color::new(255.0 / 256.0, 140.0 / 256.0, 0.0, 1.0))?;
             }
         }
@@ -211,37 +261,29 @@ impl EventHandler for MainState {
             let ch_height = map_scaler * 16.0;
 
             for layer in self.tile_image.layers.iter() {
-                for x in 0..layer.width {
-                    for y in 0..layer.height {
-                        let cell = layer.cells[y * layer.width + x];
-                        let ch = std::char::from_u32(cell.ch).unwrap();
+                let tile_iter =
+                    layer.cells.iter().enumerate().map(
+                        |(index, cell)| (index % layer.width, index / layer.width, cell)
+                     );
 
-                        let src_rect =
-                            Rect::new((cell.ch % 16) as f32 / 16.0,
-                                      (cell.ch / 16) as f32 / 16.0,
-                                      1.0 / 16.0,
-                                      1.0 / 16.0);
+                for (x, y, cell) in tile_iter {
+                    let ch = std::char::from_u32(cell.ch).unwrap();
 
-                        let pos = map_pos_to_screen(map_disp, x, y, map_scaler);
+                    let src_rect =
+                        Rect::new((cell.ch % 16) as f32 / 16.0,
+                                  (cell.ch / 16) as f32 / 16.0,
+                                  1.0 / 16.0,
+                                  1.0 / 16.0);
 
-                        let params =
-                            DrawParam::default().color(WHITE)
-                                                .dest(pos)
-                                                .src(src_rect)
-                                                .scale([map_scaler, map_scaler]);
+                    let pos = map_pos_to_screen(self.map_disp, x, y, map_scaler);
 
-                        ggez::graphics::draw(ctx, &self.font_image, params)?;
+                    let params =
+                        DrawParam::default().color(WHITE)
+                                            .dest(pos)
+                                            .src(src_rect)
+                                            .scale([map_scaler, map_scaler]);
 
-                        if (mouse_pos.x >= pos.x && mouse_pos.x < (pos.x + ch_width)) &&
-                           (mouse_pos.y >= pos.y && mouse_pos.y < (pos.y + ch_height)) {
-                                self.info.map_info =
-                                    Some(MapInfo::new(ch, x, y));
-
-                            if self.info.font_info.is_none() {
-                                self.info.font_info = Some(FontInfo::new(x as i32, y as i32, ch));
-                            }
-                        }
-                    }
+                    ggez::graphics::draw(ctx, &self.font_image, params)?;
                 }
             }
 
@@ -251,8 +293,8 @@ impl EventHandler for MainState {
                         for y in 0..layer.height {
                             let cell = layer.cells[y * layer.width + x];
                             let ch = std::char::from_u32(cell.ch).unwrap();
-                            let pos = Point2::from([map_disp.x + x as f32 * ch_width,
-                                                    map_disp.y + y as f32 * ch_height]);
+                            let pos = Point2::from([self.map_disp.x + x as f32 * ch_width,
+                                                    self.map_disp.y + y as f32 * ch_height]);
                             if ch == font_info.ch {
                                 hightlight_square(ctx, pos, ch_width, ch_height, Color::new(255.0 / 256.0, 140.0 / 256.0, 0.0, 1.0))?;
                             }
@@ -260,12 +302,12 @@ impl EventHandler for MainState {
                     }
                 }
 
-                let ch_width = font_disp.w as f32 / 16.0;
-                let ch_height = font_disp.h as f32 / 16.0;
+                let ch_width = self.font_disp.w as f32 / 16.0;
+                let ch_height = self.font_disp.h as f32 / 16.0;
                 let x = font_info.ch as usize % 16;
                 let y = font_info.ch as usize / 16;
-                let pos = Point2::from([font_disp.x + x as f32 * ch_width - 1.0,
-                                        font_disp.y + y as f32 * ch_height - 1.0]);
+                let pos = Point2::from([self.font_disp.x + x as f32 * ch_width - 1.0,
+                                        self.font_disp.y + y as f32 * ch_height - 1.0]);
                 hightlight_square(ctx, pos, ch_width, ch_height, Color::new(255.0 / 256.0, 140.0 / 256.0, 0.0, 1.0))?;
             }
         }
